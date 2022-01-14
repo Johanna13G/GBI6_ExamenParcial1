ab9ec40a4ade4458e246a7cef28373a47b335b65
Johanna13G <94419967+Johanna13G@users.noreply.github.com>
2022-01-14 14:20:23 -0500

    descripción de los comandos usados

--- Ejercicios1-3/ejercicio_1102.sh
+++ Ejercicios1-3/ejercicio_1102.sh
@@ -3,9 +3,17 @@
 
 Primero se revisa la estructura del archivo
 
+*head –n  3* para visualizar las primeras 3 filas
+
 head -n 3 Gesquiere2011_data.csv
 
+Una vez visualizado la estructura del archivo se puede observar que se necesita solo la primera columna
+
 Luego se extrae todas las columnas que contengan un 3 (o 27) y se las cuenta
+
+*grep* el cual cuenta las veces que se tiene una palabra en este caso el 3 en un archivo.
+*-w 3* para especificar que solamente cuente el número 3 y no 13 o 23
+
 
 cut -f 1 Gesquiere2011_data.csv | grep -c -w 3
 
@@ -25,6 +33,8 @@ Mediante la información del archivo count_baboons se aplica el siguiente comand
 
 bash count_baboons.sh ../data/Gesquiere2011_data.csv 27
 
+*bash* ejecuta la instrución que se encuentra en el script
+
 Dando como respuesta: 5
 
 3. Escriba un script que devuelva el número de veces que se muestreó a cada individuo.
@@ -33,7 +43,10 @@ Para esto se utilizará el siguiente comando para guardar los datos en una lista
 
 myIDS=tail -n +2 ../data/Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq
 
-Luego para mostrar el número de veces que se muestró a cada individuo se usó lo siguiente
+*sort –n* para ordenar los números
+*uniq* para que identifique solo elementos únicos
+
+Luego para mostrar el número de veces que se muestreó a cada individuo se creó un bucle con *for* permite repetir una tarea con ligeras variaciones
 
 for id in $myIDS do mycounts=bash count_baboons.sh ../data/Gesquiere2011_data.csv $id echo "ID:" $id "counts:" $mycounts done
 
