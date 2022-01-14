1ba6ae69686f3a830e409fd88c8f91c8838db056
Johanna13G <94419967+Johanna13G@users.noreply.github.com>
2022-01-14 10:49:46 -0500

    Se agregó la resolución del ejercicio 1.10.2

--- Ejercicios1-3/ejercicio_1102.sh
+++ Ejercicios1-3/ejercicio_1102.sh
@@ -1,12 +1,11 @@
-#ejercicio_1102.sh
 
-    #¿Cuántas veces se registraron los niveles de los individuos 3 y 27?
+1. ¿Cuántas veces se registraron los niveles de los individuos 3 y 27?
 
-#Primero se revisa la estructura del archivo
+Primero se revisa la estructura del archivo
 
 head -n 3 Gesquiere2011_data.csv
 
-Luego se extrae todas las columnas que contengan un 3 (o 27) y se la cuenta
+Luego se extrae todas las columnas que contengan un 3 (o 27) y se las cuenta
 
 cut -f 1 Gesquiere2011_data.csv | grep -c -w 3
 
@@ -20,7 +19,7 @@ Para 3 = 61
 
 Para 27 = 5
 
-    Escribir un script tomando como entrada el nombre del archivo y el ID del individuo,y devolver el número de registros para esa identificación.
+2. Escribir un script tomando como entrada el nombre del archivo y el ID del individuo,y devolver el número de registros para esa identificación.
 
 Mediante la información del archivo count_baboons se aplica el siguiente comando:
 
@@ -28,9 +27,9 @@ bash count_baboons.sh ../data/Gesquiere2011_data.csv 27
 
 Dando como respuesta: 5
 
-    #Escriba un script que devuelva el número de veces que se muestreó a cada individuo.
+3. Escriba un script que devuelva el número de veces que se muestreó a cada individuo.
 
-#Para esto se utilizará el siguiente comando para guardar los datos en una lista
+Para esto se utilizará el siguiente comando para guardar los datos en una lista
 
 myIDS=tail -n +2 ../data/Gesquiere2011_data.csv | cut -f 1 | sort -n | uniq
 
